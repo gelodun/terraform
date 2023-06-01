b7af2e0e4dbf1e9fd61b71f73c34e0d6a8ab4e03
Gero <54835394+gelodun@users.noreply.github.com>
2023-06-02 07:59:38 +0800

    Update ec2_terraform.tf

--- ec2_terraform.tf
+++ ec2_terraform.tf
@@ -1,8 +1,8 @@
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "PUT-YOUR-ACCESS-KEY"
-  secret_key = "PUT-YOUR-SECRET-KEY"
+  access_key = "PUT-YOUR-ACCESS-KEY-HERE"
+  secret_key = "PUT-YOUR-SECRET-KEY-HERE"
 }
 
 resource "aws_instance" "my_ec2_terraform" {
@@ -24,7 +24,7 @@ terraform {
 }
 
 provider "github" {
-  token = "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"
+  token = "PUT-YOUR-GITHUB-TOKEN-HERE"
 }
 
 resource "github_repository" "terraform" {
